342bbaafff394f365e793e8e14d69a18addfe3e0
chidori <kagakinam@gmail.com>
2018-04-05 14:38:42 +0800

    start working

---
+++ src/core/ui/DataArea.js
@@ -0,0 +1,13 @@
+goog.provide('anychart.core.ui.DataArea');
+goog.require('anychart.core.VisualBase');
+
+
+
+/**
+ * DataArea class.
+ * @constructor
+ */
+anychart.core.ui.DataArea = function() {
+  anychart.core.ui.DataArea.base(this, 'constructor');
+};
+goog.inherits(anychart.core.ui.DataArea, anychart.core.VisualBase);
